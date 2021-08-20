8e47660fcb0ae7931fa56ba0f6dd26a14d68170b
Richmond Frimpong <richmond.frimpong@smilecdr.com>
2021-08-20 10:39:22 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<ul class="votes">
+  <li class="votes list" v-for="upvote in upvotes" :key="upvote.id">
+    <up-vote :upvote="upvote"></up-vote>
+  </li>
+</ul>
+</template>
+
+<script>
+import UpVote from './components/UpVote.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UpVote
+  },
+
+  data() {
+    return{
+      upvotes: [
+        { id: 1, label: 'Default', count: 3 , img:'src/assets/Arrow+Tail-Up.svg' , selected: false },
+        { id: 2 , label: 'Normal', count: 2 , img:'src/assets/Arrow+Tail-Up-Blue.svg' , selected: false },
+        { id: 3, label: 'Fill',  count: 5 , img:'src/assets/Arrow+Tail-Up-Red.svg' , selected: false }
+      ]
+    } 
+  }
+}
+</script>
---
+++ src/components/UpVote.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="container">
+  <div class="upvote-list">
+     <div class="upvote-card">
+      <div :key="index" class="upvote-card rating" v-for="(rating , index) in item.count">
+          <div v-bind:class="{'selected': item.selected , 'not-selected': !item.selected }" class="upvote-card image-container" @click="changeBackgroudColor">
+              <img :src="item.img" class="upvote-card image" :key="item.id" :alt="'filled image'" :aria-hidden="true" />
+           </div>
+      </div>
+    </div>
+  </div>
+  <div class="upvote-buttons">
+    <button class="button-update"  @click="addRate">
+      <img src="src/assets/add-filled.svg" aria-hidden="true" title="button image"/>
+    </button>
+  </div> 
+</div>
+</template>
+<script>
+
+
+
+import Vuex, {mapState } from 'vuex';
+
+const store = new Vuex.Store({
+  state: {
+    rating:[],
+  }, getters: {
+    getRating: state => {
+      return state.rating;
+    }
+  },
+  mutations:{
+    addRating: (state, item) => {
+      state.rating.push(item);
+    },
+    addRate: (state, item) => {
+      state.rating.forEach(e => {
+         if(item.id === e.id){
+           e.count = e.count + 1
+         }
+      })
+
+    },
+
+    changeColor(state , payload) {
+      state.rating.forEach(e => {
+         if(payload === e.id){ // toogles the state from selected to not-selected
+            e.selected = !e.selected;
+         }
+      })
+    }
+  }
+});
+
+export default {
+  name: 'UpVote',
+  store,
+  props: {
+    upvote: Object
+  },
+  mounted() {
+    store.commit('addRating' , this.$props.upvote);
+  },
+  beforeDestroy() {
+    // this.sub();
+  },
+  data() {
+    return {
+      item: this.$props.upvote,
+      appliedStyles:{
+        backgroundColor: this.$props.normal
+      },
+      ratings: store.getters.getRating
+    }
+  },
+  methods: {
+    addRate(){
+      this.item.count = this.item.count + 1
+      store.commit('addRate' , this.item.id);
+    },
+    changeBackgroudColor(event) {
+      store.commit('changeColor' , this.item.id);
+    }
+  }
+
+}
+</script>
